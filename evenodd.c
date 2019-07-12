63fb3d06eed61538f315aad50b7630baf7469894
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-12 23:04:55 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,a[1000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+     if(i%2&&a[i]%2)
+     printf("%d ",a[i]);
+     if(i%2==0&&a[i]%2==0)
+     printf("%d ",a[i]);
+       }
+       
+    }
