6da662994d2b7f77603dd8e9535bb204e4a45274
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-12 22:53:41 +0530

    Create duplicate.c

---
+++ duplicate.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,a[1000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+       for(j=i+1;j<n;j++){
+           if(a[i]==a[j]){
+            
+               a[i]=-1;
+               a[j]=-1;
+              
+           }
+       }
+       
+    }
+        for(i=0;i<n;i++){
+            if(a[i]!=-1)
+            printf("%d ",a[i]);
+        }
+    
+    
+}
