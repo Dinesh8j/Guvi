9311011df8737360f22f29b5063996881d5c8a6e
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-12 23:38:43 +0530

    Update square.c

--- square.c
+++ square.c
@@ -6,8 +6,6 @@ return ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
 int main(){
 
 int t,a,b,c,d,e,f,g,h;
-scanf("%d",&t);
-while(t--){
 scanf("%d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h);
 int l1 = getDistance(a,b,c,d);
 int l2 = getDistance(c,d,e,f);
@@ -18,7 +16,6 @@ if(l1 == l3 && l2 == l4 && l1!=0){
 printf("yes");
 }else{
 printf("no");
-}	
 }
 
 return 0;
