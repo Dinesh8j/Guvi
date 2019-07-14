8d01cc138cd08e61b73e5b5a540d1c342a3d5f3d
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-14 17:16:47 +0530

    Update oddreverse.c

--- oddreverse.c
+++ oddreverse.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<string.h>
+void printstring(char* s){
+    int len=strlen(s);
+    for(int i=0;i<len;i++){
+        if(s[i]!='.')
+        printf("%c",s[i]);
+    }
+    printf(" ");
+}
 void rev(char* s){
     int len=strlen(s);
     for(int i=len-1;i>=0;i--){
+        if(s[i]!='.')
         printf("%c",s[i]);
     }
     printf(" ");
@@ -11,15 +20,15 @@ int main(){
     int i=0;
 char s[1000];
 while(scanf("%s",s)>0){
+   
     if(i%2==0){
         rev(s);
     }
     else{
-        printf("%s ",s);
+       printstring(s);
     }
     i++;
 }
-   
-    
+       
     
 }
