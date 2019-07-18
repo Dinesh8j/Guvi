e618271d367a6a913f2273fe5853af8cf054b5e4
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-18 19:31:54 +0530

    Update decode_2string_add.c

--- decode_2string_add.c
+++ decode_2string_add.c
@@ -4,7 +4,7 @@ int main()
 {
  char s1[1000],s2[1000];
  scanf("%s%s",s1,s2);
-  printf("%s\n%s\n",s1,s2);
+ // printf("%s\n%s\n",s1,s2);
  int l1=strlen(s1);
  for(int i=0;i<l1;i++){
      int sol=((s1[i]-96)+(s2[i]-96))%26+96;
