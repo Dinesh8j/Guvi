0c28f2dcbbf0a64c3ed295c5c8763c637808c7ad
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-18 17:49:56 +1200

    Create long_inc_sub_arr.c

---
+++ long_inc_sub_arr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+int i,n,a[1000],j,c=1,max=-11;
+scanf("%d",&n);
+for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+    for(j=0;j<n-1;j++){
+        while(a[j]<a[j+1]){
+            c++;
+            j++;
+        }
+        
+        if(c>max){
+        max=c;
+        c=1;
+        
+    
+}
+
+}
+printf("%d",max);
+	return 0;
+
+}
