66503b36e6a7c19efe67668965569990a65ebbef
Dinesh8j <40497875+Dinesh8j@users.noreply.github.com>
2019-07-18 17:57:56 +1200

    Update long_inc_equ_subarray.c

--- long_inc_equ_subarray.c
+++ long_inc_equ_subarray.c
@@ -7,16 +7,19 @@ for(i=0;i<n;i++){
     scanf("%d",&a[i]);
 }
     for(j=0;j<n-1;j++){
+        int same=0;
         while(a[j]<=a[j+1]){
             c++;
             j++;
+            if(a[j]==a[j+1]){
+                same++;
+            }
         }
         
         if(c>max){
-        max=c;
+        max=c-same;
         c=1;
-        
-    
+        same=0;
 }
 
 }
